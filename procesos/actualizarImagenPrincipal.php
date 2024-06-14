556c4bb92650e507700cd7051486849721328277
Juan.sartor <Juansartor92@gmail.com>
2024-06-14 19:44:01 +0200

    - agregue para q cree una imagen con otras dimensiones para la seccion ventas y alquiler - cree bien las hojas css para que detecte bien el CEO

--- procesos/actualizarImagenPrincipal.php
+++ procesos/actualizarImagenPrincipal.php
@@ -1,13 +1,76 @@
 <?php
 
-include ("../clases/conexion.php");
-$conexion= conectar();
+include("../clases/conexion.php");
+$conexion = conectar();
 
 $idInmuebleSeleccionado = $_POST['idInmuebleSeleccionado'];
 $imagenSeleccionada = $_POST['imagenSeleccionada'];
 
-$sql="UPDATE inmueble SET imagen_principal='$imagenSeleccionada' WHERE id_inmueble='$idInmuebleSeleccionado'";
-		
- mysqli_query($conexion,$sql);
+$sourcePath = '../imagenes-inmuebles/inmueble-ID-' . $idInmuebleSeleccionado . '/' . $imagenSeleccionada;
 
-?>
+
+
+
+// Obtener el tamaño original de la imagen
+list($originalWidth, $originalHeight) = getimagesize($sourcePath);
+
+// Crear una nueva imagen en blanco con el tamaño deseado
+$resizedImage = imagecreatetruecolor(335, 200);
+
+
+$imageInfo = getimagesize($sourcePath);
+
+// Obtener el tipo MIME de la imagen
+$mime = $imageInfo['mime'];
+
+$formato = '';
+// Determinar el formato en función del tipo MIME
+switch ($mime) {
+    case 'image/jpeg':
+        // Crear una imagen a partir del archivo de origen
+        $sourceImage = imagecreatefromjpeg($sourcePath); // Cambia esto si no es un JPEG
+        $formato = 'jpg';
+        break;
+    case 'image/png':
+        // Crear una imagen a partir del archivo de origen
+        $sourceImage = imagecreatefrompng($sourcePath);
+        $formato = 'png';
+        break;
+    case 'image/webp':
+        // Crear una imagen a partir del archivo de origen
+        $sourceImage = imagecreatefromwebp($sourcePath);
+        $formato = 'webp';
+        break;
+    default:
+        return 'Formato desconocido';
+}
+
+
+
+$destinationPath = '../imagenes-inmuebles/img-secciones/' . $idInmuebleSeleccionado . '.' . $formato;
+// Redimensionar la imagen
+imagecopyresampled(
+    $resizedImage,
+    $sourceImage,
+    0,
+    0,
+    0,
+    0,
+    335,
+    200,
+    $originalWidth,
+    $originalHeight
+);
+
+// Guardar la imagen redimensionada en el destino especificado
+imagejpeg($resizedImage, $destinationPath);
+
+// Liberar la memoria
+imagedestroy($sourceImage);
+imagedestroy($resizedImage);
+
+
+
+$sql = "UPDATE inmueble SET imagen_principal='$imagenSeleccionada' WHERE id_inmueble='$idInmuebleSeleccionado'";
+
+mysqli_query($conexion, $sql);
--- procesos/mostrarImagenes.php
+++ procesos/mostrarImagenes.php
@@ -1,125 +1,121 @@
 <?php
 
-include ("../clases/conexion.php");
+include("../clases/conexion.php");
 
 
 $miVariable = $_POST['miVariable'];
 
 
 // Directorio donde se encuentran las imágenes
-$directorio = '../imagenes-inmuebles/inmueble-ID-' .$miVariable."/";
+$directorio = '../imagenes-inmuebles/inmueble-ID-' . $miVariable . "/";
 
 
 // con este if verifico que el directorio existe, si existe es porque hay imagenes
-if(!is_dir($directorio)){
+if (!is_dir($directorio)) {
     echo "<b>NO SE ENCONTRARON IMAGENES!</b><br><br>";
-}
-else{
+} else {
     // Obtener la lista de archivos en el directorio
-$archivos = scandir($directorio);
+    $archivos = scandir($directorio);
 
-// Iterar sobre los archivos
-foreach ($archivos as $archivo) {
-    // Ignorar directorios y archivos ocultos
-    if ($archivo != '.' && $archivo != '..') {
-        // Generar la etiqueta <img> para cada imagen
+    // Iterar sobre los archivos
+    foreach ($archivos as $archivo) {
+        // Ignorar directorios y archivos ocultos
+        if ($archivo != '.' && $archivo != '..') {
+            // Generar la etiqueta <img> para cada imagen
 
-        $nuevo_dir= str_replace("../", "", $directorio);
+            $nuevo_dir = str_replace("../", "", $directorio);
 
-        echo '<a target="_blank" href="' . $nuevo_dir . $archivo . '"><img style="margin:2px;" width="200px" id="' . $archivo . '" src="' . $nuevo_dir . $archivo . '" alt="Imagen">
+            echo '<a target="_blank" href="' . $nuevo_dir . $archivo . '"><img style="margin:2px;" width="200px" id="' . $archivo . '" src="' . $nuevo_dir . $archivo . '" alt="Imagen">
         </a>';
 
 
-        echo '<input class="radio-principal" title="imagen principal" data-id="'.$miVariable.'" style="position: absolute;" type="radio" name="opcion" value="'.$archivo.'" > ';
+            echo '<input class="radio-principal" title="imagen principal" data-id="' . $miVariable . '" style="position: absolute;" type="radio" name="opcion" value="' . $archivo . '" > ';
 
 
 
 
-        echo ' <button title="Eliminar" class="eliminar-imagen" data-id="'. $nuevo_dir. $archivo.'" style="position:relative; bottom:150px; right:30px;" type="button" >
+            echo ' <button title="Eliminar" class="eliminar-imagen" data-id="' . $nuevo_dir . $archivo . '" style="position:relative; bottom:150px; right:30px;" type="button" >
         <span aria-hidden="true">&times;</span>
       </button>';
 
 
-      
-   
-       // echo $archivo;
+
+
+            // echo $archivo;
+        }
     }
-}
 }
 ?>
 
 <script type="text/javascript">
     /** funcion para eliminar imagen */
-    $(".eliminar-imagen").click(function(){
+    $(".eliminar-imagen").click(function() {
 
-dataId = $(this).data('id');
-rutaCompleta="../"+dataId;
-
-
-alertify.confirm('Eliminar Imagen', '¿Esta seguro que desea eliminar la imagen?',
-function(){ 
-
-$.ajax({
-url: 'procesos/deleteImage.php',
-type: 'POST',
-data: { ruta: rutaCompleta },
-success: function(response) {
-    // Manejar la respuesta del servidor si es necesario
-    console.log(response);
-    // Eliminar la imagen del DOM si la eliminación fue exitosa
-    location.reload();
-},
-error: function(xhr, status, error) {
-    // Manejar errores de AJAX si es necesario
-    console.error(error);
-}
-});
+        dataId = $(this).data('id');
+        rutaCompleta = "../" + dataId;
 
 
-}
-, function(){ });
+        alertify.confirm('Eliminar Imagen', '¿Esta seguro que desea eliminar la imagen?',
+            function() {
+
+                $.ajax({
+                    url: 'procesos/deleteImage.php',
+                    type: 'POST',
+                    data: {
+                        ruta: rutaCompleta
+                    },
+                    success: function(response) {
+                        // Manejar la respuesta del servidor si es necesario
+                        console.log(response);
+                        // Eliminar la imagen del DOM si la eliminación fue exitosa
+                        location.reload();
+                    },
+                    error: function(xhr, status, error) {
+                        // Manejar errores de AJAX si es necesario
+                        console.error(error);
+                    }
+                });
 
 
-});
-
-
-
-
-
-
-
-  // funcion para seleccionar la imagen principal
-
-  $(".radio-principal").click(function(){
-
-idInmueble = $(this).data('id');
-valorSeleccionado=$(this).val();
-
-$.ajax({
-            type:"POST",
-            data:{ idInmuebleSeleccionado: idInmueble, imagenSeleccionada:  valorSeleccionado},
-            url:"procesos/actualizarImagenPrincipal.php",
-            success:function(){
-                    
-                
-                
-                    
             },
-            error:function(){
+            function() {});
 
-                
+
+    });
+
+
+
+
+
+
+
+    // funcion para seleccionar la imagen principal
+
+    $(".radio-principal").click(function() {
+
+        idInmueble = $(this).data('id');
+        valorSeleccionado = $(this).val();
+
+        $.ajax({
+            type: "POST",
+            data: {
+                idInmuebleSeleccionado: idInmueble,
+                imagenSeleccionada: valorSeleccionado
+            },
+            url: "procesos/actualizarImagenPrincipal.php",
+            success: function() {
+
+
+
+
+            },
+            error: function() {
+
+
             }
 
         });
 
 
-
-
-
-});
-
-
-
-
-
-    </script>
+    });
+</script>
